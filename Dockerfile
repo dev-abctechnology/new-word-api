4fc1d7808849a82cf4ebc1ad522d6f7f6815741b
dev-abctechnology <60657808+jumpercs@users.noreply.github.com>
2024-05-17 12:25:04 -0300

    feat: Add Dockerfile, package dependencies, and database initialization script
    This commit adds the necessary files and configurations for containerization using Docker. The Dockerfile is included to build the application image, and the package.json file is updated to include the dependencies "dotenv" and "pg". Additionally, the init.sql file is added to initialize the database schema. This commit also updates the Dockerfile to use a newer version of the Node.js image (node:18-alpine) and removes the PostgreSQL installation step. The docker-compose.yaml file is also added to define the services and environment variables for running the application and the PostgreSQL database. Finally, the index.html file is created with the necessary HTML and JavaScript code to fetch and display a list of words from the backend API.

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,13 @@
-FROM node:16-alpine
+FROM node:18-alpine
 
 WORKDIR /app
 
-# Instalar as dependências do PostgreSQL
-RUN apk add postgresql-client
-
 COPY package*.json ./
+
 RUN npm install
 
 COPY . .
 
-# Executa o script de criação do esquema do banco de dados
-RUN psql -h your_db_host -U your_db_user -d your_db_name -f schema.sql
+EXPOSE 3000
 
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,52 +1,128 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const { Pool } = require('pg');
 const fs = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
-// Carrega o texto do livro da bíblia
-const text = fs.readFileSync('biblia.txt', 'utf8');
-// Separa as palavras do texto (utf8
-const words = text.split(/\s+/);
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+});
 
-// Caminho para o arquivo de palavras usadas
-const usedWordsFile = 'used_words.json';
+const textFile = 'biblia.txt';
 
-// Carrega as palavras usadas do arquivo (se ele existir)
-let usedWords = new Set();
-try {
-  const data = fs.readFileSync(usedWordsFile, 'utf8');
-  usedWords = new Set(JSON.parse(data));
-} catch (err) {
-  // Se o arquivo não existir, cria um novo conjunto vazio
-  //console.error('Erro ao ler o arquivo de palavras usadas:', err);
-}
-
-app.use(bodyParser.json());
-
-app.get('/api/obter-palavra', (req, res) => {
-  let nextWord;
-  let indexWord;
-  for (let i = 0; i < words.length; i++) {
-    const word = words[i];
-    if (!usedWords.has(word)) {
-      nextWord = word;
-      indexWord = i;
-      usedWords.add(word);
-      break;
+const createTableWords = async () => {
+  try {
+    const client = await pool.connect();
+    try {
+      const result = await client.query('SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = $1)', ['words']);
+      if (!result.rows[0].exists) {
+        await client.query('CREATE TABLE words (id SERIAL PRIMARY KEY, word TEXT NOT NULL, index INT NOT NULL, delivered BOOLEAN DEFAULT FALSE, date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, date_delivered TIMESTAMP, who_delivered TEXT NOT NULL)');
+        console.log('Tabela words criada com sucesso!');
+      } else {
+        console.log('Tabela words já existe!');
+      }
+    } finally {
+      client.release();
     }
+  } catch (err) {
+    console.error('Erro ao criar a tabela words:', err);
   }
+};
+
+const populateDatabase = async () => {
+  try {
+    const text = await readFileAsync(textFile, 'utf8');
+    const words = text.split(/\s+/);
+
+    const client = await pool.connect();
+    try {
+      for (let i = 0; i < words.length; i++) {
+        await client.query('INSERT INTO words (word, index, who_delivered) VALUES ($1, $2, $3)', [words[i], i, 'SYSTEM']);
+        let progress = Math.round((i + 1) / words.length * 100);
+        console.log(`Progresso: ${progress}%`);
+      }
+    } finally {
+      client.release();
+    }
+
+    console.log('Banco de dados populado com sucesso!');
+  } catch (err) {
+    console.error('Erro ao popular o banco de dados:', err);
+  }
+};
+
+const getNextWord = async () => {
+  try {
+    const client = await pool.connect();
+    try {
+      const result = await client.query('SELECT word, index FROM words WHERE delivered = FALSE ORDER BY index ASC LIMIT 1');
+      if (result.rows.length > 0) {
+        const word = result.rows[0].word;
+        const index = result.rows[0].index;
+        await client.query('UPDATE words SET delivered = TRUE, date_delivered = NOW(), who_delivered = $1 WHERE index = $2', ['USER', index]);
+        return { word, index };
+      } else {
+        return null;
+      }
+    } finally {
+      client.release();
+    }
+  } catch (err) {
+    console.error('Erro ao obter a próxima palavra:', err);
+    return null;
+  }
+};
+
+app.get('/api/obter-palavra', async (req, res) => {
+  const nextWord = await getNextWord();
 
   if (nextWord) {
-    // Salva as palavras usadas no arquivo
-    fs.writeFileSync(usedWordsFile, JSON.stringify(Array.from(usedWords)));
-    res.json({ palavra: nextWord, indice: indexWord });
+    res.json(nextWord);
   } else {
-    res.json({ mensagem: 'Não há mais palavras disponíveis.' });
+    res.status(404).json({ mensagem: 'Não há mais palavras disponíveis.' });
   }
 });
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+const waitUntilAvailable = async () => {
+  let attempts = 0;
+  const maxAttempts = 10;
+  while (attempts < maxAttempts) {
+    try {
+      const client = await pool.connect();
+      client.release();
+      console.log('Banco de dados disponível!');
+      return;
+    } catch (err) {
+      console.error(`Banco de dados indisponível. Tentativa ${attempts + 1}/${maxAttempts}`);
+      attempts++;
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+  }
+  console.error('Tempo limite excedido. O banco de dados não está disponível.');
+};
+
+waitUntilAvailable().then(() => {
+  createTableWords();
+  //verify before populating
+  pool.query('SELECT * FROM words')
+      .then((res) => {
+        if (res.rowCount === 0) {
+          populateDatabase();
+        }
+      })
+      .catch((err) => {
+        console.error('Erro ao verificar se o banco de dados está populado:', err);
+      });
 });
